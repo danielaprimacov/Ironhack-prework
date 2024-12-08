1737205e6615e3da2c1d1ebe8dd2c7573b7ed075
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2024-12-08 11:48:41 +0100

    Create reverse-vertical-print.jsx

---
+++ JS/reverse-vertical-print.jsx
@@ -0,0 +1,10 @@
+// creating a loop that prints each character of the given string, in reverse
+
+let word = "TRAMS";
+
+
+// Your code here:
+
+for (let i=word.length-1; i >= 0; i--) {
+  console.log(word[i]);
+}
