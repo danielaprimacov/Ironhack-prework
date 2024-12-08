e6a28dc126b642458a4254c9a9749636ef92affa
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2024-12-08 11:48:12 +0100

    Create vertical-print.jsx

---
+++ JS/vertical-print.jsx
@@ -0,0 +1,10 @@
+// creating a for loop that prints each character of the  given string
+let bootcamp = "IRONHACK";
+
+
+// Your code here:
+
+for (let i=0; i <= bootcamp.length -1; i++)
+  {
+    console.log(bootcamp[i]);
+  }
