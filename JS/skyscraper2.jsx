546bcdddc24e9fe1166202e4accff2f0902cfc54
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2024-12-08 11:50:10 +0100

    Create skyscraper2.jsx

---
+++ JS/skyscraper2.jsx
@@ -0,0 +1,8 @@
+// an outer loop that runs 4 times
+for (let i = 0; i < 4; i++) {
+  console.log("+-+-+-+-+-+-+");
+  // inner loop that runs 2 times
+  for (let i = 0; i < 2; i++) {
+    console.log("| 0 0 0 0 0 |");
+  }
+}
