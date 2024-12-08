d42428daca3daee7b730b9d7af00bc349a37c7e2
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2024-12-08 11:53:36 +0100

    Create dog-years-converter.jsx

---
+++ JS/dog-years-converter.jsx
@@ -0,0 +1,8 @@
+// a function that takes the number of dog years and converts it to human years
+function convertDogYears(dogYears) {
+  let humanYears = dogYears * 7;
+  return humanYears;
+}
+
+const result = convertDogYears(11);
+console.log(result);
