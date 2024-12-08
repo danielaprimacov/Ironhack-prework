25d6810cf804c93a662abc0bce9bda14126eb89d
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2024-12-08 11:53:02 +0100

    Create final-countdown.jsx

---
+++ JS/final-countdown.jsx
@@ -0,0 +1,9 @@
+// a loop that will print all the numbers from 15 to 0, counting backward
+for (let i = 15; i >= 0; i--) {
+  console.log(i);
+  if (i === 11) {
+    console.log("Final countdown");
+  } else if (i === 0 ) {
+    console.log("Take off!"); 
+  } 
+}
