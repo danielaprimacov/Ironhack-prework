d07ec34a4e3e57c8abfe9f011fc967e726a49670
Daniela Primacov <108431426+danielaprimacov@users.noreply.github.com>
2024-12-08 11:49:41 +0100

    Create skyscraper1.jsx

---
+++ JS/skyscraper1.jsx
@@ -0,0 +1,8 @@
+// an outer loop that run 3 times
+for (let i = 0; i < 3; i++) {
+  console.log("+------+");
+  // inner loop that runs 3 times
+  for (let i = 0; i < 3; i++) {
+    console.log("| o o |");
+  }
+}
